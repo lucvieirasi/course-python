e80f26c0c55d548ac2091535295e46024c00052b
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-10 13:00:52 -0300

    Initial commit

---
+++ dir_e_help.py
@@ -0,0 +1,4 @@
+## Utilirarios Python para auxiliar na programacao
+## Dir -> Apresenta todos os tipos de atributos/propriedades e funcoes/metodos disponiveis para determinado tipo de dado ou
+## Help - > Apresenta a documentacao/como utilizar os atributos/propriedade e funcoes/metodos disponiveis para determinado
+## tipo de dado ou variavel.
