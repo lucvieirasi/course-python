6b8bbbc3c3010e43cac6b75119c298584871dc3d
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-11 17:36:24 -0300

    inputs and outputs.

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,13 @@
+
+# Exemplo  print versao 2.Python
+#
+# Exemplo de print atual 3.6
+print('Qual o seu nome?')
+nome = input()
+print(f'Seja bem-vindo(a) {nome}')
+print('Qual a sua idade?')
+idade = input()
+print(f'A {nome} tem {idade} anos...')
+print(f'A {nome} nasceu em {2018 - int(idade)} ..')
+# Cast é a conversao de um tipo de dado para outro.
+
