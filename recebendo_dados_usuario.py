e3606de238a8665b47ff7f341b057a29fe900125
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-11 17:39:55 -0300

    inputs and outputs.

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -1,6 +1,16 @@
 
 # Exemplo  print versao 2.Python
-#
+# Em Python, string é tudo que estiver entre:
+# - Aspas simples;
+# - Aspas duplas;
+# - Aspas simples triplas;
+# - Aspas duplas triplas;
+# Exemplos:
+# -Aspas Simples ==> 'Joaozinho'
+# -Aspas duplas ==> "Joaozinho"
+# -Aspas Simples triplas ==> '''Joaozinho'''
+# -Aspas duplas triplas ==> """Joaozinho"""
+
 # Exemplo de print atual 3.6
 print('Qual o seu nome?')
 nome = input()
@@ -8,6 +18,9 @@ print(f'Seja bem-vindo(a) {nome}')
 print('Qual a sua idade?')
 idade = input()
 print(f'A {nome} tem {idade} anos...')
-print(f'A {nome} nasceu em {2018 - int(idade)} ..')
+
 # Cast é a conversao de um tipo de dado para outro.
+# Exemplo:
+print(f'A {nome} nasceu em {2018 - int(idade)} ..')
+
 
