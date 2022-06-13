0cec47675c2085bb547bf7c4661591ec92f5f21f
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-13 11:59:29 -0300

    type string.

--- tipo_string.py
+++ tipo_string.py
@@ -10,3 +10,23 @@ nome = 'Greek University'
 print(nome)
 print(type(nome))
 
+nome = "Gina's Bar"
+print(nome)
+print(type(nome))
+
+nome = 'Angelina Jolie'
+print(nome)
+print(type(nome))
+
+nome = """ Angelina
+Jolie"""
+print(nome)
+print(type(nome))
+
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+nome = 'Geek University'
+print(nome[0:4]) #Slice de string
+
+print(nome[5:15]) #Slice de string
+print(nome.split()[0])
