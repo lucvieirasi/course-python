d188f77e1f1ce351c9d440776acbd1b215a453c2
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-13 11:37:13 -0300

    type string.

---
+++ tipo_string.py
@@ -0,0 +1,12 @@
+"""
+Tipo string
+Em Python, um dado é considerado do tipo string semrpe que:
+- Estiver entre Áspas simples -> 'uma string', '234', 'a', 'True', '42.3'
+- Esiver entre áspas duplas ->  "uma string", "234", "a", "True", "42.3"
+- Estiver entre áspas simples triplas -> ''uma string'', ''234'', ''a'', ''True'', ''42.3''
+- Estiver entre aspas simples triplas -> '''uma string''', '''234''', '''a''', '''True''', '''42.3'''
+"""
+nome = 'Greek University'
+print(nome)
+print(type(nome))
+
