1e2c1e4cdd69186de2f6984ce2234a4b96081cef
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-13 10:02:45 -0300

    numerical types.

--- tipo_float.py
+++ tipo_float.py
@@ -16,9 +16,14 @@ valor = 1.44
 print(valor)
 print(type(valor))
 
-# é possível fazer dupla atribuicao
+#É possível fazer dupla atribuicao
 valor1, valor2 = 1, 44
 print(valor1)
 print(type(valor1))
 print(valor2)
 print(type(valor2))
+
+# Podemos converter um float para um int
+res = int(valor)
+print(res)
+print(type(res))
