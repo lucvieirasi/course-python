3c4c4492ed2e5d8499dfadf4daf337a1405076b6
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-13 09:49:38 -0300

    numerical types.

--- tipo_float.py
+++ tipo_float.py
@@ -6,12 +6,19 @@ Tipo Real . Decimal
 Casas Decimais
 OBS: O separador de casas decimais na programacao é ponto e náo a virgula.
 """
-# Errado
+# Errado do ponto de vista do Float,mas gera uma tupla.
 valor = 1,44
 print(valor)
 print(type(valor))
 
-# Certo
+# Certo, do ponto de vista do Float.
 valor = 1.44
 print(valor)
 print(type(valor))
+
+# é possível fazer dupla atribuicao
+valor1, valor2 = 1, 44
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
