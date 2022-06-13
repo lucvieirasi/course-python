4678d7ed7f2abeaf29228ec156fca2c343721f3e
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-13 09:47:00 -0300

    numerical types.

---
+++ tipo_float.py
@@ -0,0 +1,17 @@
+""""
+Tipo Float
+
+Tipo Real . Decimal
+
+Casas Decimais
+OBS: O separador de casas decimais na programacao é ponto e náo a virgula.
+"""
+# Errado
+valor = 1,44
+print(valor)
+print(type(valor))
+
+# Certo
+valor = 1.44
+print(valor)
+print(type(valor))
